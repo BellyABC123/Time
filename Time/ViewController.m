7e9bca5597b41bdfb789a8290aa84d536865438e
sammovale <sammovale@hotmail.com>
2013-05-26 12:15:57 +0930

    Changed background to brown
    yes it does look like shit now.

--- Time/ViewController.m
+++ Time/ViewController.m
@@ -25,7 +25,7 @@
     [super viewDidLoad];
     [self setTime];
     
-    [self.view setBackgroundColor:[UIColor blackColor]]; //example code for changing background
+    [self.view setBackgroundColor:[UIColor brownColor]]; //example code for changing background
     
     
     [currentLocation setFont:[UIFont fontWithName:@"Quicksand" size:26]];
